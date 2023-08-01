0ec15d1b1a8b5f87ee4a5ee9b038437795c59366
Yang <heuyang@gmail.com>
2023-08-01 21:43:33 +0930

    Added OrderApi solution and projects (Except for API project)

---
+++ HappyMenu.OrderApi/HappyMenu.OrderApi.Service/v1/Command/UpdateOrderCommand.cs
@@ -0,0 +1,11 @@
+﻿using HappyMenu.OrderApi.Domain;
+using MediatR;
+using System.Collections.Generic;
+
+namespace HappyMenu.OrderApi.Service.v1.Command
+{
+    public class UpdateOrderCommand : IRequest
+    {
+        public List<Order> Orders { get; set; }
+    }
+}
---
+++ HappyMenu.OrderApi/HappyMenu.OrderApi.Service/v1/Query/GetPaidOrderQuery.cs
@@ -0,0 +1,10 @@
+﻿using HappyMenu.OrderApi.Domain;
+using MediatR;
+using System.Collections.Generic;
+
+namespace HappyMenu.OrderApi.Service.v1.Query
+{
+    public class GetPaidOrderQuery : IRequest<List<Order>>
+    {
+    }
+}
