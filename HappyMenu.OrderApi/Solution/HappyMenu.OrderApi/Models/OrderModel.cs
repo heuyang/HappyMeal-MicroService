bf752977e5b5f7a8fc235f7703cdf50ca1660af1
Yang <heuyang@gmail.com>
2023-08-03 22:08:31 +0930

    Add OrderAPI project

---
+++ HappyMenu.OrderApi/Solution/HappyMenu.OrderApi/Models/OrderModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace HappyMenu.OrderApi.Models
+{
+    public class OrderModel
+    {
+        [Required] public Guid CustomerGuid { get; set; }
+
+        [Required] public string CustomerFullName { get; set; }
+    }
+}
---
+++ HappyMenu.OrderApi/Solution/HappyMenu.OrderApi/Validators/v1/OrderModelValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using HappyMenu.OrderApi.Models;
+
+namespace HappyMenu.OrderApi.Validators.v1
+{
+    public class OrderModelValidator : AbstractValidator<OrderModel>
+    {
+        public OrderModelValidator()
+        {
+            RuleFor(x => x.CustomerFullName)
+                .NotNull()
+                .WithMessage("The customer name must be at least 2 character long");
+            RuleFor(x => x.CustomerFullName)
+                .MinimumLength(2).WithMessage("The customer name must be at least 2 character long");
+        }
+    }
+}
